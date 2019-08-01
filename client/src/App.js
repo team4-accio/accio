82d354bd6262730a3b3be2c7200028420b20d480
arg <anna.gaukel@gmail.com>
2019-08-01 18:39:24 -0500

    re-structure for testing

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,23 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Wrapper from "./components/Wrapper";
 import AdminInventory from "./components/Pages/admin-inventory/AdminInventory";
 import TestPage from "./components/testPage/testPage";
+import User from "./components/Pages/User";
 
 
 function App() {
     return (
-        <Router>
-            <div>
-                {/* THIS IS FOR TESTING, CHANGE ROUTING / EXPRESS LATER */}
-                <Wrapper>
-                    <Route exact path="/" component={TestPage} />
-                    <Route exact path="/admin/inventory" component={AdminInventory} />
-                </Wrapper>
-            </div>
-        </Router>
+        // <Router>
+        //     <div>
+        //         {/* THIS IS FOR TESTING, CHANGE ROUTING / EXPRESS LATER */}
+        //         <Wrapper>
+        //             <Route exact path="/" component={TestPage} />
+        //             <Route exact path="/admin/inventory" component={AdminInventory} />
+        //         </Wrapper>
+        //     </div>
+        // </Router>
+        <div>
+            <User />
+        </div>
     );
 }
 
