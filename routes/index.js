456d1c470cad302b853ef368cb700199351eee37
justin <jlouie10@gmail.com>
2019-08-01 16:26:29 -0500

    Add React app path

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Dependencies
+const path = require('path');
 const router = require('express').Router(); // Create a Router instance
 
 // Require all API routes
@@ -8,5 +9,10 @@ const api = require('./api');
 
 // API Routes
 router.use('/api', api);
+
+// If no API routes are hit, send React app
+router.use(function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 module.exports = router;
