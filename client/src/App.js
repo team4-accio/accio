da44fcc429dd205b9fe457ccd214f2ac86a98859
arg <anna.gaukel@gmail.com>
2019-08-01 20:40:13 -0500

    display only user index for test

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Wrapper from "./components/Wrapper";
-import AdminInventory from "./components/Pages/admin-inventory/AdminInventory";
-import TestPage from "./components/testPage/testPage";
+// import AdminInventory from "./components/Pages/admin-inventory/AdminInventory";
+// import TestPage from "./components/testPage/testPage";
 import User from "./components/Pages/User";
 
 
 function App() {
     return (
-        // <Router>
-        //     <div>
-        //         {/* THIS IS FOR TESTING, CHANGE ROUTING / EXPRESS LATER */}
-        //         <Wrapper>
-        //             <Route exact path="/" component={TestPage} />
-        //             <Route exact path="/admin/inventory" component={AdminInventory} />
-        //         </Wrapper>
-        //     </div>
-        // </Router>
-        <div>
-            <User />
-        </div>
+        <Router>
+            <div>
+                {/* THIS IS FOR TESTING, CHANGE ROUTING / EXPRESS LATER */}
+                <Wrapper>
+                    {/* <Route exact path="/" component={TestPage} />
+                    <Route exact path="/admin/inventory" component={AdminInventory} /> */}
+                    <User />
+                </Wrapper>
+
+            </div>
+
+        </Router>
+
     );
 }
 
