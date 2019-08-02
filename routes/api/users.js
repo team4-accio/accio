e7eadfb8b4063194358fff6327e6c08644ca000b
justin <jlouie10@gmail.com>
2019-08-02 01:33:20 -0500

    Add deep population for items on users response

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,7 +12,11 @@ router.route('/')
     // GET route for listing all users sorted by id, with the most recent users appearing first
     .get(function (req, res) {
         db.User.find(req.body)
-            .populate({ path: 'checkouts', options: { sort: { _id: -1 } } })
+            .populate({
+                path: 'checkouts',
+                populate: { path: 'items' },
+                options: { sort: { _id: -1 } }
+            })
             .sort({ _id: -1 })
             .then(function (user) {
                 res.status(200).json(user);
@@ -37,7 +41,11 @@ router.route('/:_id')
     // GET route for retrieving a user by id
     .get(function (req, res) {
         db.User.findById(req.params._id)
-            .populate({ path: 'checkouts', options: { sort: { _id: -1 } } })
+            .populate({
+                path: 'checkouts',
+                populate: { path: 'items' },
+                options: { sort: { _id: -1 } }
+            })
             .then(function (user) {
                 res.status(200).json(user);
             })
@@ -48,7 +56,11 @@ router.route('/:_id')
     // PATCH route for updating a user by id
     .patch(function (req, res) {
         db.User.findOneAndUpdate({ _id: req.params._id }, req.body, { new: true })
-            .populate({ path: 'checkouts', options: { sort: { _id: -1 } } })
+            .populate({
+                path: 'checkouts',
+                populate: { path: 'items' },
+                options: { sort: { _id: -1 } }
+            })
             .then(function (user) {
                 res.status(200).json(user);
             })
