5aafbad0984fd6d2e97e979563b51bba1ae00338
Zeke <ezekielbadowski@gmail.com>
2019-08-02 11:23:18 -0500

    Moved AdminInvent testArr into separate file

--- client/src/components/Pages/admin-inventory/AdminInventory.js
+++ client/src/components/Pages/admin-inventory/AdminInventory.js
@@ -1,87 +1,22 @@
 import React, { Component } from "react";
 import M from "materialize-css";
 import CollapseBody from "./local-components/CollapseBody";
+import axios from "axios";
+import testArr from "./testArr.json"
 
-const testArr = [
-    {
-        category: 'Laptop - PC',
-        serialNumber: '11111',
-        condition: 'Good',
-        available: true,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        tags: []
-    },
-    {
-        category: 'Laptop - PC',
-        serialNumber: '22222',
-        condition: 'Okay',
-        available: false,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        tags: []
-    },
-    {
-        category: 'Laptop - PC',
-        serialNumber: '33333',
-        condition: 'Bad',
-        available: true,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        tags: []
-    },
-    {
-        category: 'Laptop - Mac',
-        serialNumber: '44444',
-        condition: 'Good',
-        available: true,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        tags: []
-    },
-    {
-        category: 'Laptop - Mac',
-        serialNumber: '55555',
-        condition: 'Okay',
-        available: true,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        tags: []
-    },
-    {
-        category: 'Laptop - Mac',
-        serialNumber: '66666',
-        condition: 'Bad',
-        available: false,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        tags: []
-    },
-    {
-        category: 'iPad',
-        serialNumber: '77777',
-        condition: 'Good',
-        available: true,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        tags: []
-    },
-    {
-        category: 'iPad',
-        serialNumber: '88888',
-        condition: 'Okay',
-        available: true,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        tags: []
-    },
-    {
-        category: 'iPad',
-        serialNumber: '99999',
-        condition: 'Bad',
-        available: false,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        tags: []
-    }
-]
 class AdminInventory extends Component {
     state = {
         sortedItems: {}
     }
 
     componentDidMount() {
+        // axios.get('/items')
+        //     .then(function (response) {
+        //         console.log(response);
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error);
+        //     });
         this.sortItems(testArr)
         M.AutoInit();
     }
