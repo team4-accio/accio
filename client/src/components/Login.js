42e3967bc3b55ef363882dce396909edc45c40ca
jimmy <rucker>
2019-08-06 13:43:39 -0500

    Login.js Updateed
    Axios added to login comopnent

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,29 +1,59 @@
 import React from "react";
+import axios from "axios";
 
-function Login() {
-  return (
-    <div class="container">
-      <div class="row">
-        <div class="col s10">
-          <form method="post" action="" class="forms">
-            <label>
-              Email <span class="req">*</span>
-              <input type="email" name="user-email" class="width-50" />
-            </label>
+export default class Login extends React.Component {
+  state = {
+    useremail: "",
+    userpassword: ""
+  };
 
-            <label>
-              Password
-              <input type="password" name="user-password" class="width-50" />
-            </label>
-            <p>
-              <button class="btn btn-blue">Log in</button>
-              <button class="btn">Cancel</button>
-            </p>
-          </form>
+  handleChange = event => {
+    this.setState({ useremail: event.target.value });
+    this.setState({ userpassword: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      useremail: this.state.useremail,
+      userpassword: this.state.userpassword
+    };
+
+    axios
+      .post(`https://jsonplaceholder.typicode.com/users`, { user })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <div class="container">
+          <div class="row">
+            <div class="col s10" />
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                Email <span class="req">*</span>
+                <input type="email" name="useremail" class="width-50" />
+              </label>
+
+              <label>
+                Password
+                <input type="password" name="userpassword" class="width-50" />
+              </label>
+              <p>
+                <button button type="submit" class="btn btn-blue">
+                  Log in
+                </button>
+                <button class="btn">Cancel</button>
+              </p>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default Login;
