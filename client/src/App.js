b3feda93b6e86bebd5205dce5356746f02b201e7
jimmy <rucker>
2019-08-06 10:57:05 -0500

    App Updated
    Home.js added to App.js for home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Wrapper from "./components/Wrapper";
 import AdminInventory from "./components/Pages/admin-inventory/AdminInventory";
-import TestPage from "./components/testPage/testPage";
-import AdminAction from "./components/Pages/admin-action/AdminAction"
-
+import TestPage from "./components/Pages/Home";
+import AdminAction from "./components/Pages/admin-action/AdminAction";
 
 function App() {
-    return (
-        <Router>
-            <div>
-                {/* THIS IS FOR TESTING, CHANGE ROUTING / EXPRESS LATER */}
-                <Wrapper>
-                    <Route exact path="/" component={TestPage} />
-                    <Route exact path="/admin/action" component={AdminAction} />
-                    <Route exact path="/admin/inventory" component={AdminInventory} />
-                </Wrapper>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div>
+        {/* THIS IS FOR TESTING, CHANGE ROUTING / EXPRESS LATER */}
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/admin/action" component={AdminAction} />
+          <Route exact path="/admin/inventory" component={AdminInventory} />
+        </Wrapper>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
