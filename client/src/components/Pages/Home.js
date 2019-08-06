2dccd7d5f623e66390a8370155a2449eca5abb42
jimmy <rucker>
2019-08-06 10:54:54 -0500

    Home Updated
    Home updated

---
+++ client/src/components/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Header";
+import Login from "../Login";
+import Footer from "../Footer";
+
+function Home() {
+  return (
+    <div className="App">
+      <Header />
+      <Login />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
