6ff1a90a3c44f78648c5d926b2446e4c13bb061f
justin <jlouie10@gmail.com>
2019-08-10 08:50:04 -0500

    Add retrieve session user method

---
+++ app/auth/session.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// Dependencies
+const router = require('express').Router(); // Create a Router instance
+
+// Require User model
+const User = require('../users').model;
+
+// Require utilities
+const utils = require('../utils');
+
+// Routes
+// Matches with /session
+router.route('/')
+    // GET route to retrieve session user with token header
+    .get(function (req, res) {
+        if (req.headers['x-session-token']) {
+            User.findOne({ session: req.headers['x-session-token'] })
+                .populate({
+                    path: 'checkouts',
+                    populate: { path: 'items' },
+                    options: { sort: { _id: -1 } }
+                })
+                .then(function (user) {
+                    if (user) {
+                        res.status(200).json(utils.format.usersResponse(user));
+                    } else {
+                        res.status(401).json({
+                            error: {
+                                code: 401,
+                                message: 'Unauthorized request'
+                            }
+                        });
+                    }
+                })
+                .catch(function (err) {
+                    res.status(500).json(err);
+                });
+        } else {
+            res.status(401).json({
+                error: {
+                    code: 401,
+                    message: 'Unauthorized request'
+                }
+            });
+        }
+    });
+
+module.exports = router;
