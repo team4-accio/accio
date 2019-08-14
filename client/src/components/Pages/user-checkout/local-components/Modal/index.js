6eae8daba28ab57e58c3eb2aa2b8ad4fe072fe5c
arg <anna.gaukel@gmail.com>
2019-08-14 18:17:20 -0500

    update modal with click function to handle post

--- client/src/components/Pages/user-checkout/local-components/Modal/index.js
+++ client/src/components/Pages/user-checkout/local-components/Modal/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CartCard from "../CartCard";
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
 // import ptBR from 'date-fns/locale/pt-BR';
 // registerLocale('pt-BR', ptBR);
 // import "./style.css";
@@ -11,7 +12,7 @@ class Cart extends Component {
         console.log(props);
         super(props);
         this.state = {
-            startDate: new Date(),
+            returnDate: new Date(),
         }
         this.handleDateChange = this.handleDateChange.bind(this);
     }
@@ -21,8 +22,35 @@ class Cart extends Component {
     // }
     handleDateChange(date) {
         this.setState({
-            startDate: date
+            returnDate: date
         });
+        // console.log(date);
+    }
+
+    handleCheckout() {
+        // var idsOnly = []
+        // for (let i = 0; i < this.props.carts.length; i++) {
+        //     idsOnly.push(this.props.carts[i]._id)
+        // }
+
+        var payload = {
+            items: this.props.carts.map(each => each._id),
+            out: new Date(),
+            return: this.state.returnDate,
+            status: "pending",
+            user: "5d422915501c450bd4230aac"
+        }
+        console.log(payload)
+        axios.post("/api/checkouts", payload,
+            {
+                headers: {
+                    authorization: "86b89440-bb1d-11e9-8a28-0f10265f69af"
+                }
+            }
+        ).then(function (data) {
+            console.log(data)
+        });
+
     }
 
     render() {
@@ -43,11 +71,11 @@ class Cart extends Component {
                         </div>
                     </div>
                     <DatePicker
-                        selected={this.state.startDate}
+                        selected={this.state.returnDate}
                         onChange={this.handleDateChange}
                     />
                     <div className="modal-footer">
-                        <a href="#!" className="modal-close waves-effect waves-green btn-flat">Checkout</a>
+                        <button onClick={() => this.handleCheckout()} className="modal-close waves-effect waves-green btn-flat">Checkout</button>
                     </div>
                 </div>
             </div>
