280129131fa558c7e8b0bbc79a069328a9f4c1d9
Zeke <ezekielbadowski@gmail.com>
2019-08-14 07:27:54 -0500

    Converted Condition Chart to Sunburst

--- client/src/components/Pages/admin-dashboard/local-components/DashInventory/index.js
+++ client/src/components/Pages/admin-dashboard/local-components/DashInventory/index.js
@@ -30,7 +30,7 @@ class DashInventory extends Component {
         super(props)
         this.state = {
             waitingForCondition: true,
-            conditionData: [],
+            conditionData: {},
             inCount: 0,
             outCount: 0,
             waitingForAvailability: true,
@@ -45,61 +45,106 @@ class DashInventory extends Component {
 
     componentDidMount() {
         // M.AutoInit();
-        this.getItemConditions()
-        this.getItemAvailability()
+        API.getItems().then(results => {
+            this.getItemConditions(results.data)
+            this.getItemAvailability(results.data)
+        })
+        
 
     }
 
-    getItemConditions() {
-        API.searchItems('condition', 'new')
-            .then((res) => {
-                //this.setState({ newCount: res.data.length })
-                setTimeout(this.setState(prevState => ({
-                    conditionData: [...prevState.conditionData, {
-                        angle: res.data.length,
-                        color: "00ccff"
-                    }]
-                })), 1000);
-            })
-        API.searchItems('condition', 'good')
-            .then((res) => {
-                //this.setState({ goodCount: res.data.length })
-                this.setState(prevState => ({
-                    conditionData: [...prevState.conditionData, {
-                        angle: res.data.length,
-                        color: "green"
-                    }]
-                }))
-            })
-        API.searchItems('condition', 'okay')
-            .then((res) => {
-                //this.setState({ okayCount: res.data.length })
-                this.setState(prevState => ({
-                    conditionData: [...prevState.conditionData, {
-                        angle: res.data.length,
-                        color: "yellow"
-                    }]
-                }))
-            })
-        API.searchItems('condition', 'bad')
-            .then((res) => {
-                this.setState(prevState => ({
-                    conditionData: [...prevState.conditionData, {
-                        angle: res.data.length,
-                        color: "red"
-                    }]
-                }))
-                this.setState({ waitingForCondition: false })
-            })
-        // I don't like putting wFC above in case of API error, but running into async issues below 
-        // this.setState({ waitingForCondition: false })
+    getItemConditions(items) {
+        // API.searchItems('condition', 'new')
+        //     .then((res) => {
+        //         //this.setState({ newCount: res.data.length })
+        //         setTimeout(this.setState(prevState => ({
+        //             conditionData: [...prevState.conditionData, {
+        //                 angle: res.data.length,
+        //                 color: "00ccff"
+        //             }]
+        //         })), 1000);
+        //     })
+        // API.searchItems('condition', 'good')
+        //     .then((res) => {
+        //         //this.setState({ goodCount: res.data.length })
+        //         this.setState(prevState => ({
+        //             conditionData: [...prevState.conditionData, {
+        //                 angle: res.data.length,
+        //                 color: "green"
+        //             }]
+        //         }))
+        //     })
+        // API.searchItems('condition', 'okay')
+        //     .then((res) => {
+        //         //this.setState({ okayCount: res.data.length })
+        //         this.setState(prevState => ({
+        //             conditionData: [...prevState.conditionData, {
+        //                 angle: res.data.length,
+        //                 color: "yellow"
+        //             }]
+        //         }))
+        //     })
+        // API.searchItems('condition', 'bad')
+        //     .then((res) => {
+        //         this.setState(prevState => ({
+        //             conditionData: [...prevState.conditionData, {
+        //                 angle: res.data.length,
+        //                 color: "red"
+        //             }]
+        //         }))
+        //         this.setState({ waitingForCondition: false })
+        //     })
+        // // I don't like putting wFC above in case of API error, but running into async issues below 
+        // // this.setState({ waitingForCondition: false })
+        let obj = {}
+                for (let i in items) {
+                    if (obj[items[i].category]) {
+                        obj[items[i].category][items[i].condition+'Count']++
+                    }
+                    else {
+                        obj[items[i].category] = {
+                            newCount: 0,
+                            goodCount: 0,
+                            okayCount:0,
+                            badCount:0,
+                        }
+                        obj[items[i].category][items[i].condition+'Count']++
+                    }
+                }
+                this.createConditionData(obj)
+    }
+    createConditionData(itemObj){
+        let data = {
+            "title": "Categories",
+            "clr": "#12939A",
+            "children": []
+        }
+        Object.keys(itemObj).map((keyName, keyIndex) => {
+            let tempObj = {
+                bigness: 1,
+                "title": keyName,
+                "clr": COLORS[keyIndex],
+                "children": [
+                    { bigness: 1, "title": "new", "clr": "blue", "size": itemObj[keyName].newCount },
+                    { bigness: 1, "title": "good", "clr": "green", "size": itemObj[keyName].goodCount },
+                    { bigness: 1, "title": "okay", "clr": "yellow", "size": itemObj[keyName].okayCount },
+                    { bigness: 1, "title": "bad", "clr": "red", "size": itemObj[keyName].badCount }
+                ],
+                labelStyle: {
+                    fontSize: 15,
+                    fontWeight: 'bold'
+                }
+            }
+            data.children.push(tempObj)
+        })
+        this.setState({ conditionData: data, waitingForCondition: false })
     }
 
 
-    getItemAvailability() {
-        API.getItems()
-            .then((results) => {
-                let items = results.data
+    getItemAvailability(items) {
+        //API.getItems()
+          //  .then((results) => {
+           //     let items = results.data
                 let obj = {}
 
                 for (let i in items) {
@@ -119,7 +164,7 @@ class DashInventory extends Component {
                     }
                 }
                 this.createAvailablityData(obj)
-            })
+            //})
     }
     // Turns the counted in/out items for each category into a usable object for the sunburst graph
     createAvailablityData(itemObj) {
@@ -178,7 +223,7 @@ class DashInventory extends Component {
                                         alignItems: 'center',
                                         justifyContent: 'center'
                                     }}>
-                                    <RadialChart
+                                    {/* <RadialChart
                                         data={this.state.conditionData}
                                         innerRadius={100}
                                         radius={140}
@@ -187,7 +232,34 @@ class DashInventory extends Component {
                                         // animation only works when new data is passed into it, state change refreshes whole component
                                         // setTimeout seems to be a quick fix
                                         animation
-                                        colorType="literal" />
+                                        colorType="literal" /> */}
+                                         <Sunburst
+                                        hideRootNode
+                                        data={this.state.conditionData}
+
+                                        style={{ stroke: '#fff' }}
+                                        onValueMouseOver={v =>
+                                            this.setState({ hoveredCell: v.x && v.y ? v : false })
+                                        }
+                                        onValueMouseOut={v => this.setState({ hoveredCell: false })}
+                                        height={300}
+                                        margin={{ top: 50, bottom: 50, left: 50, right: 50 }}
+                                        getLabel={d => d.name}
+                                        //getSize={d => d.bigness}
+                                        getColor={d => d.clr}
+                                        width={350}
+                                        padAngle={() => 0.0}
+                                        colorType="literal"
+                                    >
+                                        {hoveredCell ? (
+                                            <Hint value={this.buildValue(hoveredCell)}>
+                                                <div style={tipStyle}>
+                                                    <div style={{ ...boxStyle, background: hoveredCell.clr }} />
+                                                    {hoveredCell.title}
+                                                </div>
+                                            </Hint>
+                                        ) : null}
+                                    </Sunburst>
                                     <div style={{
                                         position: 'absolute',
                                         padding: '5px',
