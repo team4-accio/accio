e68a24b6836487c2572e83f2872df8a038a2bf37
justin <jlouie10@gmail.com>
2019-08-17 12:06:55 -0500

    Update authenticate to be a middleware function

--- app/auth/session.js
+++ app/auth/session.js
@@ -16,39 +16,37 @@ const utils = require('../utils');
 // Matches with /session
 router.route('/')
     // GET route to retrieve session user with token header
-    .get(function (req, res) {
-        auth.authorize(req, res, function () {
-            if (req.headers['x-session-token']) {
-                User.findOne({ session: req.headers['x-session-token'] })
-                    .populate({
-                        path: 'checkouts',
-                        populate: { path: 'items' },
-                        options: { sort: { _id: -1 } }
-                    })
-                    .then(function (user) {
-                        if (user) {
-                            res.status(200).json(utils.format.usersResponse(user));
-                        } else {
-                            res.status(401).json({
-                                error: {
-                                    code: 401,
-                                    message: 'Unauthorized request'
-                                }
-                            });
-                        }
-                    })
-                    .catch(function (err) {
-                        res.status(500).json(err);
-                    });
-            } else {
-                res.status(401).json({
-                    error: {
-                        code: 401,
-                        message: 'Unauthorized request'
+    .get(auth.authenticate, function (req, res) {
+        if (req.headers['x-session-token']) {
+            User.findOne({ session: req.headers['x-session-token'] })
+                .populate({
+                    path: 'checkouts',
+                    populate: { path: 'items' },
+                    options: { sort: { _id: -1 } }
+                })
+                .then(function (user) {
+                    if (user) {
+                        res.status(200).json(utils.format.usersResponse(user));
+                    } else {
+                        res.status(401).json({
+                            error: {
+                                code: 401,
+                                message: 'Unauthorized request'
+                            }
+                        });
                     }
+                })
+                .catch(function (err) {
+                    res.status(500).json(err);
                 });
-            }
-        });
+        } else {
+            res.status(401).json({
+                error: {
+                    code: 401,
+                    message: 'Unauthorized request'
+                }
+            });
+        }
     });
 
 module.exports = router;
