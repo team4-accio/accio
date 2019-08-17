9ff99914195f04bba8b4fadd736efb2c9eb6f715
Zeke <ezekielbadowski@gmail.com>
2019-08-17 12:33:10 -0500

    Finished Inventory component; animations and state changes

--- client/src/components/Pages/admin-dashboard/local-components/DashInventory/index.js
+++ client/src/components/Pages/admin-dashboard/local-components/DashInventory/index.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import M from "materialize-css";
 import "./style.css";
 import API from "../../../../../utils/API";
-import "../../../../../../node_modules/react-vis/dist/style.css"
+import "../../../../../../node_modules/react-vis/dist/style.css";
 import SunburstChart from "../SunburstChart";
 
 // 12 colors for sunburst categories 
 // if inventory has more than 12 categories will have an error
-const COLORS = ["#D4F1F4", "#75E6DA", "#00ccff", "#189AB4", "#05445E", "#4682B4", "#008081", "#0080FF", "#0E4D92", "#1034A6", "#003152", "#1D2951"]
+const COLORS = ["#D4F1F4", "#75E6DA", "#00ccff", "#189AB4", "#05445E", "#4682B4", "#008081", "#0080FF", "#0E4D92", "#1034A6", "#003152", "#1D2951"];
 
 class DashInventory extends Component {
     constructor(props) {
@@ -19,24 +19,20 @@ class DashInventory extends Component {
             outCount: 0,
             waitingForAvailability: true,
             availablityData: {},
-            conditionHoverTitle: ""
+            conditionHoverTitle: " ",
+            availablityHoverTitle: " "
         }
     }
 
-    // componentWillReceiveProps(nextProps) {
-    //     // this.setState({ tagList: nextProps.tags })
-    // }
-
     componentDidMount() {
-        // M.AutoInit();
         API.getItems().then(results => {
-            this.sortData(results.data)
+            this.sortData(results.data);
         })
     }
     // sorts data from DB to be parsed by the createData functions for the chart to use
-    sortData(items){
-        let condObj = {}
-        let availObj = {}
+    sortData(items) {
+        let condObj = {};
+        let availObj = {};
 
         for (let i in items) {
             if (condObj[items[i].category]) {
@@ -52,7 +48,7 @@ class DashInventory extends Component {
                     okayCount: 0,
                     badCount: 0,
                 }
-                condObj[items[i].category][items[i].condition + 'Count']++
+                condObj[items[i].category][items[i].condition + 'Count']++;
 
                 availObj[items[i].category] = {
                     inCount: 0,
@@ -63,8 +59,8 @@ class DashInventory extends Component {
                     : availObj[items[i].category].outCount++
             }
         }
-        this.createConditionData(condObj)
-        this.createAvailablityData(availObj)
+        this.createConditionData(condObj);
+        this.createAvailablityData(availObj);
     }
 
     createConditionData(itemObj) {
@@ -72,57 +68,59 @@ class DashInventory extends Component {
             "title": "Categories",
             "clr": "#12939A",
             "children": []
-        }
+        };
         Object.keys(itemObj).map((keyName, keyIndex) => {
             let tempObj = {
                 bigness: 1,
                 "title": keyName,
                 "clr": COLORS[keyIndex],
                 "children": [
-                    { bigness: 1, "title": "new", "clr": "blue", "size": itemObj[keyName].newCount },
-                    { bigness: 1, "title": "good", "clr": "green", "size": itemObj[keyName].goodCount },
-                    { bigness: 1, "title": "okay", "clr": "yellow", "size": itemObj[keyName].okayCount },
-                    { bigness: 1, "title": "bad", "clr": "red", "size": itemObj[keyName].badCount }
+                    { bigness: 1, title: "new", clr: "blue", size: itemObj[keyName].newCount },
+                    { bigness: 1, title: "good", clr: "green", size: itemObj[keyName].goodCount },
+                    { bigness: 1, title: "okay", clr: "yellow", size: itemObj[keyName].okayCount },
+                    { bigness: 1, title: "bad", clr: "red", size: itemObj[keyName].badCount }
                 ],
                 labelStyle: {
                     fontSize: 15,
                     fontWeight: 'bold'
                 }
-            }
-            data.children.push(tempObj)
+            };
+            data.children.push(tempObj);
         })
-        this.setState({ conditionData: data, waitingForCondition: false })
+        this.setState({ conditionData: data, waitingForCondition: false });
     }
 
- 
+
     // Turns the counted in/out items for each category into a usable object for the sunburst graph
     createAvailablityData(itemObj) {
         let data = {
-            "title": "Categories",
-            "clr": "#12939A",
-            "children": []
-        }
+            title: "Categories",
+            clr: "#12939A",
+            children: []
+        };
         Object.keys(itemObj).map((keyName, keyIndex) => {
             let tempObj = {
                 bigness: 1,
-                "title": keyName,
-                "clr": COLORS[keyIndex],
-                "children": [
-                    { bigness: 1, "title": "out", "clr": "red", "size": itemObj[keyName].outCount },
-                    { bigness: 1, "title": "in", "clr": "green", "size": itemObj[keyName].inCount }
+                title: keyName,
+                clr: COLORS[keyIndex],
+                children: [
+                    { bigness: 1, title: "out", clr: "red", "size": itemObj[keyName].outCount },
+                    { bigness: 1, title: "in", clr: "green", "size": itemObj[keyName].inCount }
                 ],
                 labelStyle: {
                     fontSize: 15,
                     fontWeight: 'bold'
                 }
-            }
-            data.children.push(tempObj)
-        })
-        this.setState({ availablityData: data, waitingForAvailability: false })
+            };
+            data.children.push(tempObj);
+        });
+        this.setState({ availablityData: data, waitingForAvailability: false });
     }
-    getHoverTitle(title, source){
+    getHoverTitle(title, source) {
         //console.log(title)
-        this.setState({conditionHoverTitle: title})
+        source === "condition"
+            ? this.setState({ conditionHoverTitle: title })
+            : this.setState({ availablityHoverTitle: title })
     }
 
 
@@ -133,48 +131,64 @@ class DashInventory extends Component {
             <div className="conatainer">
                 <div className="card">
                     {/* <a href='/admin/inventory'> */}
-                        <div className="card-content ">
-                            <span className="card-title grey-text text-darken-4 center-align">Inventory</span>
-                            <div className="divider" />
+                    <div className="card-content ">
+                        <span className="card-title grey-text text-darken-4 center-align">Inventory</span>
+                        <div className="divider" />
 
-                            {this.state.waitingForCondition
-                                ? (<div className="progress">
-                                    <div className="indeterminate"></div>
-                                </div>)
-                                : (<div
-                                    style={{
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'center'
-                                    }}>
-                                    <SunburstChart data={this.state.conditionData} {...this.state} getHoverTitle={(title, source) => this.getHoverTitle(title, source)}/>
-                                    <div style={{
-                                        position: 'absolute',
-                                        padding: '5px',
-                                    }}>Condition <br />
-                                    {this.state.conditionHoverTitle} </div>
-                                </div>)
-                            }
+                        {this.state.waitingForCondition
+                            ? (<div className="progress">
+                                <div className="indeterminate"></div>
+                            </div>)
+                            : (<div
+                                style={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center'
+                                }}>
+                                <SunburstChart
+                                    chartName="condition"
+                                    data={this.state.conditionData}
+                                    {...this.state}
+                                    getHoverTitle={(title, source) => this.getHoverTitle(title, source)}
+                                />
+                                <div style={{
+                                    position: 'absolute',
+                                    padding: '5px',
+                                }}>
+                                    <p className="center">Condition</p>
+                                    <p className="center" >{this.state.conditionHoverTitle}</p>
+                                </div>
 
-                            {this.state.waitingForAvailability
-                                ? (<div className="progress">
-                                    <div className="indeterminate"></div>
-                                </div>)
-                                : (<div
-                                    style={{
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'center'
-                                    }}>
-                                    <SunburstChart data={this.state.availablityData} />
-                                    <div style={{
-                                        position: 'absolute',
-                                        padding: '5px',
-                                    }}>Availability</div>
-                                </div>)
-                            }
+                            </div>)
+                        }
 
-                        </div>
+                        {this.state.waitingForAvailability
+                            ? (<div className="progress">
+                                <div className="indeterminate"></div>
+                            </div>)
+                            : (<div
+                                style={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center'
+                                }}>
+                                <SunburstChart
+                                    chartName="availability"
+                                    data={this.state.availablityData}
+                                    {...this.state}
+                                    getHoverTitle={(title, source) => this.getHoverTitle(title, source)}
+                                />
+                                <div style={{
+                                    position: 'absolute',
+                                    padding: '5px',
+                                }}>
+                                    <p className="center">Availabity</p>
+                                    <p className="center" >{this.state.availablityHoverTitle}</p>
+                                </div>
+                            </div>)
+                        }
+
+                    </div>
                     {/* </a> */}
 
                 </div>
--- client/src/components/Pages/admin-dashboard/local-components/SunburstChart/index.js
+++ client/src/components/Pages/admin-dashboard/local-components/SunburstChart/index.js
@@ -3,96 +3,21 @@ import {
     Sunburst,
     Hint,
 } from 'react-vis';
+import dummyData from "./initialData.json";
 
-const tipStyle = {
-    display: 'flex',
-    color: '#000',
-    // background: '#000',
-    opacity: "0.6",
-    alignItems: 'center',
-    padding: '5px'
-};
-const boxStyle = { height: '10px', width: '10px' };
-
-
-function buildValue(hoveredCell) {
-    const { radius, angle, angle0 } = hoveredCell;
-    const truedAngle = (angle + angle0) / 2;
-    return {
-        x: radius * Math.cos(truedAngle),
-        y: radius * Math.sin(truedAngle)
-    };
-}
 export default class SunburstChart extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            chartName: props.chartName,
             hoveredCell: false,
             hoveredTitle: "",
-            data: {
-                "title": "Dummy Data",
-                "clr": "#0012939A",
-                "children": [
-                    {
-                        bigness: 1,
-                        "title": "Dummy Data",
-                        "clr": "transparent",
-                        "size": 1,
-                        children: [
-                            {
-                                bigness: 1,
-                                "title": "Dummy Data",
-                                "clr": "transparent",
-                                "size": 2000,
-                                children: [
-                                    {
-                                        bigness: 1,
-                                        "title": "Dummy Data",
-                                        "clr": "transparent",
-                                        "size": 2000
-                                    }
-                                ]
-                            },
-                            {
-                                bigness: 1,
-                                "title": "Dummy Data",
-                                "clr": "transparent",
-                                "size": 2000
-                            }
-                        ]
-                    },
-                    {
-                        bigness: 1,
-                        "title": "Dummy Data",
-                        "clr": "transparent",
-                        "size": 1,
-                        children: [
-                            {
-                                bigness: 1,
-                                "title": "Dummy Data",
-                                "clr": "transparent",
-                                "size": 2000
-                            },
-                            {
-                                bigness: 1,
-                                "title": "Dummy Data",
-                                "clr": "transparent",
-                                "size": 2000
-                            }
-                        ]
-                    }]
-            }
+            data: dummyData
         }
     }
     componentDidMount() {
         setTimeout(() => this.setState({ data: this.props.data }), 100);
     }
-
-    // componentWillReceiveProps(nextProps) {
-    //     console.log(nextProps.data)
-    //     //this.setState({data: nextProps.data})
-    //     setTimeout(() => this.setState({ data: nextProps.data }), 100);
-    // }
 
     render() {
         const { hoveredCell, data } = this.state;
@@ -104,12 +29,14 @@ export default class SunburstChart extends React.Component {
 
                 style={{ stroke: '#fff' }}
                 onValueMouseOver={v => {
-                    //console.log(v)
-                    this.props.getHoverTitle(v.title ? v.title : "", 'condition')
-                    this.setState({ hoveredCell: true, hoveredTitle: v.title ? v.title : "" })
+                    this.props.getHoverTitle(v.title ? v.title : "", this.state.chartName);
+                    this.setState({ hoveredCell: true, hoveredTitle: v.title ? v.title : "" });
                 }
                 }
-                onValueMouseOut={v => this.setState({ hoveredTitle: "", hoveredCell: false })}
+                onValueMouseOut={v => {
+                    this.props.getHoverTitle(" ", this.state.chartName);
+                    this.setState({ hoveredTitle: "", hoveredCell: false });
+                }}
                 height={300}
                 margin={{ top: 50, bottom: 50, left: 50, right: 50 }}
                 getLabel={d => d.name}
@@ -119,18 +46,7 @@ export default class SunburstChart extends React.Component {
                 padAngle={() => 0.0}
                 colorType="literal"
                 animation
-            >
-{/* 
-                <Hint value={{
-                    x: 1,
-                    y: 0
-                }}>
-                    <div style={tipStyle}>
-                        {this.state.hoveredTitle}
-                    </div>
-                </Hint> */}
-
-            </Sunburst>
+            />
 
         );
     }
