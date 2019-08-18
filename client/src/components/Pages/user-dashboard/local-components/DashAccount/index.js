575ffdc8726f4b6b9318db0fee5e318d9aaed467
Zeke <ezekielbadowski@gmail.com>
2019-08-17 22:25:00 -0500

    Some clean up

--- client/src/components/Pages/user-dashboard/local-components/DashAccount/index.js
+++ client/src/components/Pages/user-dashboard/local-components/DashAccount/index.js
@@ -13,18 +13,14 @@ export default function DashAccount(props) {
 
     return (
         <div className="conatainer">
-
             <div className="card">
                 <a onClick={openTapTarget}>
                     <div className="card-content ">
                         <i className="material-icons " style={{ fontSize: '20vw' }}>face</i>
-
                         <span className="card-title grey-text text-darken-4 center-align">Account</span>
-
                     </div>
                 </a>
             </div>
-
         </div>
     );
 
--- client/src/components/Pages/user-dashboard/local-components/DashCheckout/index.js
+++ client/src/components/Pages/user-dashboard/local-components/DashCheckout/index.js
@@ -8,9 +8,7 @@ export default function DashUsers(props) {
                 <a href='/users/checkout'>
                     <div className="card-content ">
                         <i className="material-icons " style={{ fontSize: '20vw' }}>add_shopping_cart </i>
-
                         <span className="card-title grey-text text-darken-4 center-align">Checkout</span>
-
                     </div>
                 </a>
             </div>
