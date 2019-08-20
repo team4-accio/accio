5bf2dced64488f2a5617a497c2fccddbf4522cb7
justin <jlouie10@gmail.com>
2019-08-20 14:56:17 -0500

    Remove cancel button, remove console logs and move axios request to API.js from login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,85 +1,74 @@
-import React from "react";
+import React, { Component } from 'react';
+import API from '../utils/API';
 
-import axios from "axios";
-
-export default class Login extends React.Component {
-  state = {
-    email: "",
-    password: ""
-  };
-
-  // handleChange = event => {
-  //   this.setState({ email: event.target.value, password: event.target.value });
-  //   console.log(event.target.value);
-  // };
-
-  handleChange = event => {
-    // Getting the value and name of the input which triggered the change
-    const { name, value } = event.target;
-    // Updating the input's state
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-    const user = {
-      email: this.state.email,
-      password: this.state.password
+class Login extends Component {
+    state = {
+        email: '',
+        password: ''
     };
-    console.log(user);
 
-    axios
-      .post(`/login`, user)
-      .then(res => {
-        console.log(res.headers["x-session-token"]);
-        console.log(res);
-        localStorage.setItem("sessionid", res.headers["x-session-token"]);
-        console.log("storage", localStorage.getItem("sessionid"));
-        // console.log(res.headers);
-        // console.log(res.data);
-      })
-      .catch(err => console.log(err));
-  };
+    handleChange = event => {
+        // Getting the value and name of the input which triggered the change
+        const { name, value } = event.target;
+        // Updating the input's state
+        this.setState({
+            [name]: value
+        });
+    };
 
-  render() {
-    return (
-      <div>
-        <div className="container">
-          <div className="row">
-            <div className="col s10" />
-            <form onSubmit={this.handleSubmit}>
-              <label>
-                Email <span className="req">*</span>
-                <input
-                  type="email"
-                  name="email"
-                  className="width-50"
-                  onChange={this.handleChange}
-                />
-              </label>
+    handleSubmit = event => {
+        event.preventDefault();
 
-              <label>
-                Password
-                <input
-                  type="password"
-                  name="password"
-                  className="width-50"
-                  onChange={this.handleChange}
-                />
-              </label>
-              <p>
-                <button type="submit" className="btn btn-blue">
-                  Log in
-                </button>
-                <button className="btn">Cancel</button>
-              </p>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        API.login({
+            email: this.state.email,
+            password: this.state.password
+        })
+            .then(res =>
+                localStorage.setItem(
+                    'sessionid',
+                    res.headers['x-session-token']
+                )
+            )
+            .catch(err => console.log(err));
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col s10" />
+                        <form onSubmit={this.handleSubmit}>
+                            <label>
+                                Email <span className="req">*</span>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    className="width-50"
+                                    onChange={this.handleChange}
+                                />
+                            </label>
+
+                            <label>
+                                Password
+                                <input
+                                    type="password"
+                                    name="password"
+                                    className="width-50"
+                                    onChange={this.handleChange}
+                                />
+                            </label>
+                            <p>
+                                <button type="submit" className="btn btn-blue">
+                                    Log in
+                                </button>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
+
+export default Login;
