7eafb8564c81432c1615a47217a6bbb842963056
jimmy <rucker>
2019-08-19 23:04:21 -0500

    Created logout button
    Created logout link

---
+++ client/src/components/common/HeaderLogout.js
@@ -0,0 +1,52 @@
+import React from "react";
+import axios from "axios";
+
+export default class Login extends React.Component {
+  state = {
+    id: ""
+  };
+
+  handleChange = event => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = event.target;
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    console.log(user);
+
+    axios
+      .delete(`/login`, user)
+      .then(res => {
+        console.log(res.headers["x-session-token"]);
+        console.log(res);
+        localStorage.setItem("sessionid", res.headers["x-session-token"]);
+        console.log("storage", localStorage.getItem("sessionid"));
+        // console.log(res.headers);
+        // console.log(res.data);
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <input type="text" name="id" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Delete</button>
+        </form>
+      </div>
+    );
+  }
+}
