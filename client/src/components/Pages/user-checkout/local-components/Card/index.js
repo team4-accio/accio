c2eda556e20969d32bbb4698e1c152ace5207d35
arg <anna.gaukel@gmail.com>
2019-08-19 21:57:37 -0500

    remove column for render in dashboard

--- client/src/components/Pages/user-checkout/local-components/Card/index.js
+++ client/src/components/Pages/user-checkout/local-components/Card/index.js
@@ -7,20 +7,21 @@ import "./style.css";
 function Card(props) {
     return (
         <div className="row">
-            <div className="col s12 m6">
-                <div className="card blue-grey darken-1">
-                    <div className="card-content white-text">
-                        <span className="card-title"><p>{props.category}  <button onClick={() => props.handleRequest(props.id)}>Request</button></p></span>
+            {/* <div className="col s12 m6"> */}
+            <div className="card blue-grey darken-1">
+                <div className="card-content white-text">
+                    <span className="card-title"><p>{props.name}  <button className="waves-effect waves-light btn" onClick={() => props.handleRequest(props.id)}>Request</button></p></span>
 
-                    </div>
-                    <div className="card-action">
+                </div>
+                <div className="card-action">
+                    <p>Info: {props.description}</p>
+                    <p>{props.condition}</p>
 
-                        <p>{props.condition}</p>
-                        {/* <a href="#">This is a link</a>
+                    {/* <a href="#">This is a link</a>
                         <a href="#">This is a link</a> */}
-                    </div>
                 </div>
             </div>
+            {/* </div> */}
         </div >
     )
 }
